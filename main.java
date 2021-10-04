046b6ba55258830565dc0f1d7aa9e3ec259f6934
Nzeanochieh Chukwuebuka <46981746+nzeanochieh@users.noreply.github.com>
2021-10-04 13:28:32 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package guitinz;
+
+import javax.swing.JFrame;
+
+public class bukus {
+public static void main(String[] arg){
+	Gui go=new Gui();
+	go.setTitle("Prof Nzeanochieh Ebuka's CGPA Calculator");
+	go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	go.setSize(1000,500);
+	go.setVisible(true);
+}
+}
